f3ea0109bf6633d3a88e507fa6355e62a095c279
Matt-Joshua Tan <mattjoshuatan@gmail.com>
2024-06-03 14:04:06 -0400

    v1

--- main.py
+++
@@ -1,7 +0,0 @@
-from src.spotify import Spotify
-
-def main():
-    pass
-
-if __name__ == "__main__":
-    main()
--- src/spotify.py
+++
@@ -1,9 +0,0 @@
-# some shit boutta go here
-
-class Spotify:
-    
-    def __init__(self, key="KEYHERE"):
-        pass
-
-    
-    def get(self):
